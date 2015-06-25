04fea4cf862179c9d12b9d0944c3afbc7306927f
Daniel Hsu <danieljhsu@gmail.com>
2015-06-24 17:47:56 -0700

    modified schema.sql to make user FB id a String

--- schema.sql
+++ schema.sql
@@ -1,12 +1,12 @@
-CREATE DATABASE numifyserver;
+-- CREATE DATABASE numifyserver;
 
-\c numifyserver;
+-- \c numifyserver;
 
 CREATE TABLE users (
 	id SERIAL PRIMARY KEY NOT NULL,
 	name varchar(100) NOT NULL,
 	email varchar(255),
-	user_FB_id INT NOT NULL,
+	user_FB_id varchar(255) NOT NULL,
 	user_created timestamp DEFAULT localtimestamp NOT NULL
 );
 
